6f045e857e15399a2c23dcaadeefd90eb0d1b7ae
AYTOS\fernando.villen <fernando.villansolis@berger-levrault.com>
2025-06-02 09:15:05 +0200

    feat: tablero de juego

--- frontend/src/App-React/App.tsx
+++ frontend/src/App-React/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Home from './pages/home/Home';
+import PaginaJuegoEsquivar from './pages/PaginaJuegoEsquivar';
 
 const App = () => (
   <Routes>
@@ -13,6 +14,8 @@ const App = () => (
 
     {/* Página de login */}
     <Route path="/login" element={<Login />} />
+
+    <Route path="/juego_esquivar" element={<PaginaJuegoEsquivar />} />
 
   </Routes>
 );
---
+++ frontend/src/App-React/pages/PaginaJuegoEsquivar.tsx
@@ -0,0 +1,174 @@
+import { Grid } from "@mui/material";
+
+export default function PaginaEsquivar() {
+    return(
+        <Grid container height={"100vh"} width={"100vw"} alignItems={"stretch"} sx={{}}>
+            {/* Row 1 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>1x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>1x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>1x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>1x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>1x5</p>
+                </Grid>
+            </Grid>
+
+            {/* Row 2 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>2x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>2x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>2x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>2x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>2x5</p>
+                </Grid>
+            </Grid>
+
+            {/* Row 3 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>3x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>3x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>3x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>3x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>3x5</p>
+                </Grid>
+            </Grid>
+
+            {/* Row 4 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>4x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>4x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>4x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>4x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>4x5</p>
+                </Grid>
+            </Grid>
+
+            {/* Row 5 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>5x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>5x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>5x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>5x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>5x5</p>
+                </Grid>
+            </Grid>
+
+            {/* Row 6 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>6x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>6x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>6x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>6x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>6x5</p>
+                </Grid>
+            </Grid>
+            {/* Row 7 */}
+            <Grid container size={12}  justifyContent={"center"} alignItems={"center"} >
+                {/*Column 1*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>7x1</p>
+                </Grid>
+                {/*Column 2*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>7x2</p>
+                </Grid>
+                {/*Column 3*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>7x3</p>
+                </Grid>
+                {/*Column 4*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>7x4</p>
+                </Grid>
+                {/*Column 5*/}
+                <Grid size={2} display={"flex"} border={1} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+                    <p>7x5</p>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
