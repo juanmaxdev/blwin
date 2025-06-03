7f16ddfe6aab5ecfe9c805bf15db3f3e89451175
AYTOS\manuel.gomez <mj.gomezreina@berger-levrault.com>
2025-06-03 14:48:17 +0200

    test: Creación de tests de Controlador

---
+++ backend/src/Ble.Triviados/Test.BLWin/Controllers/TestsUsuarioJuegoControllers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.BLWin.Controllers
+{
+    [TestClass]
+    public class TestsUsuarioJuegoControllers
+    {
+
+        [TestMethod]
+        public async Task RegistrarPuntuacionCorrectamente()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+    }
+}
