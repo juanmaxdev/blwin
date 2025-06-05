627a8a4550cec585f6dc037bd4aaab510d2bd331
AYTOS\antonio.cantero <a.canterosantaella@berger-levrault.com>
2025-06-05 09:13:56 +0200

    feature: separación de test de servicios en una carpeta independiente

--- backend/src/Ble.Triviados/Test.BLWin/Services/TestsJuegoService.cs
+++ backend/src/Ble.Triviados/Test.BLWin/Services/TestsJuegoService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 
-namespace Ble.Triviados.Tests
+namespace Test.BLWin.Services
 {
     [TestClass]
     public class JuegoServiceTests
