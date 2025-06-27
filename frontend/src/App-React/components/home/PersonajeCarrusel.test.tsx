6ecb2dbf6cc70cbc39bc0e19d1d69ba7f44bc960
AYTOS\francisco.marin <fj.marinalvarez@berger-levrault.com>
2025-06-27 11:41:57 +0200

    fix: test de home arreglados

--- frontend/src/App-React/components/home/PersonajeCarrusel.test.tsx
+++ frontend/src/App-React/components/home/PersonajeCarrusel.test.tsx
@@ -6,10 +6,10 @@ test('muestra el personaje seleccionado al hacer clic', () => {
   render(<PersonajeCarrusel />);
   
   // Clic en el personaje "Robotín v2.0"
-  const imagen = screen.getByAltText('Robotín v2.0');
+  const imagen = screen.getByAltText('Z-Wing');
   fireEvent.click(imagen);
 
   // Se espera ver su nombre y parte de su historia
-  expect(screen.getByText('Robotín v2.0')).toBeInTheDocument();
-  expect(screen.getByText(/Forjado en los laboratorios/i)).toBeInTheDocument();
+  expect(screen.getByText('Z-Wing')).toBeInTheDocument();
+  expect(screen.getByText(/Las naves Z-Wing surcan el espacio en busca de conocimiento y descubrimientos. Hoy es tu primer día como piloto, y el universo entero se abre ante ti. ¡Veamos hasta dónde puedes llegar!/i)).toBeInTheDocument();
 });
